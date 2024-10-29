5b73d701810ccbdfbb06fc7b175fb95534e6ed95
Shivam Pandey <shivampandey4194@gmail.com>
2024-10-29 15:44:02 +0530

    Initial commit

---
+++ TokenType.java
@@ -0,0 +1,14 @@
+public enum TokenType {
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    LEFT_BRACKET,
+    RIGHT_BRACKET,
+    COLON,
+    COMMA,
+    STRING,
+    NUMBER,
+    TRUE,
+    FALSE,
+    NULL,
+    EOF
+}
